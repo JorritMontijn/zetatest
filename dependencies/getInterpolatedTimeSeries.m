07bd2c10b82795dd95536ccf7d3937b329037c8c
Jorrit Montijn <jorritmontijn@gmail.com>
2023-09-20 14:35:19 +0200

    Update getInterpolatedTimeSeries.m

--- dependencies/getInterpolatedTimeSeries.m
+++ dependencies/getInterpolatedTimeSeries.m
@@ -9,6 +9,14 @@ function matTracePerTrial = getInterpolatedTimeSeries(vecTimestamps,vecData,vecE
 	%1.0 - June 26 2019
 	%	Created by Jorrit Montijn
 	
+    %% check which interp1 bypass to use
+    strNew=which('matlab.internal.math.interp1');
+    if ~isempty(strNew)
+        boolUseNew=true;
+    else
+        boolUseNew=false;
+    end
+
 	%% assign data
 	matTracePerTrial = nan(numel(vecEventStartT),numel(vecRefT));
 	for intTrial=1:numel(vecEventStartT)
@@ -24,9 +32,15 @@ function matTracePerTrial = getInterpolatedTimeSeries(vecTimestamps,vecData,vecE
 		vecUseInterpT = vecRefT+dblStartT;
 		
 		%get real fractions for training set
-        vecInterpTrace = interp1(vecUseTimes,vecUseTrace,vecUseInterpT);
+        %vecInterpTrace = interp1(vecUseTimes,vecUseTrace,vecUseInterpT);
+
         %bypass time-consuming checks and use direct method
-        %vecInterpTrace = matlab.internal.math.interp1(vecUseTimes,vecUseTrace,'linear','linear',vecUseInterpT);
+        if boolUseNew
+            vecInterpTrace = matlab.internal.math.interp1(vecUseTimes,vecUseTrace,'linear','linear',vecUseInterpT);
+        else
+            F = griddedInterpolant(X,V,'linear');
+            vecInterpTrace = F(Xqcol);
+        end
 		matTracePerTrial(intTrial,:) = vecInterpTrace;
 	end
 end
