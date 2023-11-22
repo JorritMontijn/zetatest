7034f26b63a6a49248fe3c883d7d8c84d98cbf9a
Jorrit Montijn <jorritmontijn@gmail.com>
2023-11-22 19:27:03 +0100

    Revert "test kuiper-like"
    This reverts commit 0d98b173d32cb430ae5f49240a643c67281ab9dc.

--- dependencies/getZetaDev.m
+++
@@ -1,9 +0,0 @@
-function [dblZetaDev,dblZetaDev_Inv,intZetaDevIdx,intZetaDevIdx_Inv] = getZetaDev(vecIn)
-	%getZetaDev Returns +ZETA and -ZETA
-	%   [dblZetaDev,dblZetaDev_Inv,intZetaDevIdx,intZetaDevIdx_Inv] = getZetaDev(vecIn)
-	
-	[dummy,intZetaDevIdx]=max(abs(vecIn));
-	dblZetaDev = vecIn(intZetaDevIdx);
-	[dblZetaDev_Inv,intZetaDevIdx_Inv]=max(vecIn*-sign(dblZetaDev));
-end
-
