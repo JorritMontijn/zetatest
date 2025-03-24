416a05982c38334c2a8bae34b8db263430e333e9
Alexander Heimel <alexander.heimel@gmail.com>
2025-03-24 17:45:24 +0100

    fixed bugfix of getUniqueSpikes

--- dependencies/getUniqueSpikes.m
+++ dependencies/getUniqueSpikes.m
@@ -3,15 +3,16 @@ function vecSpikeTimes = getUniqueSpikes(vecSpikeTimes)
 	vecSpikeTimes = sort(vecSpikeTimes);
 	%dblUniqueOffset = max(eps(vecSpikeTimes));
 	dblUniqueOffset = eps(class(vecSpikeTimes)); % to match Python version
+    dblShift = dblUniqueOffset;
 	indDuplicates = [false;diff(vecSpikeTimes)<dblUniqueOffset];
 	while any(indDuplicates)
 		vecNotUnique = vecSpikeTimes(indDuplicates);
 		vecJitter = cat(1,1+9*rand([numel(vecNotUnique),1]),-1-9*rand([numel(vecNotUnique),1]));
-		vecJitter = dblUniqueOffset*vecJitter(my_randperm(numel(vecJitter),numel(vecNotUnique)));
+		vecJitter = dblShift*vecJitter(my_randperm(numel(vecJitter),numel(vecNotUnique)));
 		vecSpikeTimes(indDuplicates) = vecSpikeTimes(indDuplicates) + vecJitter;
 		vecSpikeTimes = sort(vecSpikeTimes);
 		indDuplicates = [false;diff(vecSpikeTimes)<dblUniqueOffset];
-        dblUniqueOffset = dblUniqueOffset * 2; % to avoid endless loop if vecJitter is too small
+        dblShift = dblShift * 2; % to avoid endless loop if vecJitter is too small
     end
 end
 
